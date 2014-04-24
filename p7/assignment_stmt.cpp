2a62ee4afd18b14e945132cbe9abeb69abbfac3b
Erik Mellum <emellum@mail.csuchico.edu>
2014-04-23 18:45:33 -0700

    Stuck on test 51. Only 23 tests left

--- p7/assignment_stmt.cpp
+++ p7/assignment_stmt.cpp
@@ -34,25 +34,44 @@ void Assignment_stmt::execute()
 		break;
 		case PLUS_EQUALS:
 			if(lhs->eval()->getType() == INT)
-			{
 				lhs->eval()->setInt(rhs->eval_int() + lhs->eval()->getInt());
-			}
 			else if(lhs->eval()->getType() == DOUBLE)
 			{
 				if(rhs->getGplType() == DOUBLE || rhs->getGplType() == INT)
 					lhs->eval()->setDouble(rhs->eval_double() + lhs->eval()->getDouble());
 			}
+			else if(lhs->eval()->getType() == STRING)
+			{
+				if(rhs->getGplType() == DOUBLE || rhs->getGplType() == INT || rhs->getGplType() == STRING)
+					lhs->eval()->setString(lhs->eval()->getString() + rhs->eval_string());
+			}
+			else if(lhs->eval()->getType() == GAME_OBJECT)
+				if(lhs->getType() == OBJECT_PARAM)
+				{
+					if(lhs->getParamType(lhs->eval()) == INT)
+						lhs->setIntParam(lhs->eval(), rhs->eval_int() + lhs->getIntValue(lhs->eval()));
+					else if(lhs->getParamType(lhs->eval()) == DOUBLE)
+						lhs->setDoubleParam(lhs->eval(), rhs->eval_double() + lhs->getDoubleValue(lhs->eval()));
+					else if(lhs->getParamType(lhs->eval()) == STRING)
+						lhs->setStringParam(lhs->eval(), lhs->getStringValue(lhs->eval()) + rhs->eval_string());
+				}
 		break;
 		case MINUS_EQUALS:
 			if(lhs->eval()->getType() == INT)
-			{
 				lhs->eval()->setInt(lhs->eval()->getInt() - rhs->eval_int());
-			}
 			else if(lhs->eval()->getType() == DOUBLE)
 			{
 				if(rhs->getGplType() == DOUBLE || rhs->getGplType() == INT)
 					lhs->eval()->setDouble(lhs->eval()->getDouble() - rhs->eval_double());
 			}
+			else if(lhs->eval()->getType() == GAME_OBJECT)
+				if(lhs->getType() == OBJECT_PARAM)
+				{
+					if(lhs->getParamType(lhs->eval()) == INT)
+						lhs->setIntParam(lhs->eval(), lhs->getIntValue(lhs->eval()) - rhs->eval_int());
+					else if(lhs->getParamType(lhs->eval()) == DOUBLE)
+						lhs->setDoubleParam(lhs->eval(), lhs->getDoubleValue(lhs->eval()) - rhs->eval_double());
+				}
 		break;
 	}
 }
